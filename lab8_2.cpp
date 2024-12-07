658b41ff9ed593c0220c3b3b57d4eedc188b0219
Champ <champsrw49@gmail.com>
2024-12-07 15:52:48 +0700

    Lab8_2 complete

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,7 +3,19 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 75) {
+        return 'B';
+    } else if (score > 60) {
+        return 'C';
+    } else if (score > 45) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +29,13 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
+		getline(cin, name[i]);
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+	
 		cout << "Score of student " << i+1 << ": ";
+		cin >> score[i];
 		//[Missing Code 3] Get score of the i-th students.
+		
 		i++;
 	}
 	
